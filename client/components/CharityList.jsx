72c7cb3550417bceb135bf995dded9853ea40015
Kara Raithe <55517663+kraithe@users.noreply.github.com>
2021-08-27 15:45:44 -0400

    Implement Fragment-based rendering of components

--- client/components/CharityList.jsx
+++ client/components/CharityList.jsx
@@ -4,6 +4,8 @@ import CharityModal from './CharityModal.jsx';
 // 1. Render the 3 images of charities on the main page
 // 2. On click, show the modal for a specific charity
 
+// props - all 1-3 charities, all their info!
+
 class CharityList extends React.Component {
   constructor(props) {
     super(props);
@@ -13,37 +15,45 @@ class CharityList extends React.Component {
     this.toggleModal = this.toggleModal.bind(this);
   }
 
+  // Conditionally render CharityModal
   toggleModal(i) {
-    const modalBool = this.state.isOpen;
-    this.setState({ isOpen: !modalBool, photoIndex: i });
+    const modalBool = this.state.modalOpen;
+    this.setState({ modalOpen: !modalBool});
+  }
+
+  openModal() {
+    <React.Fragment>
+      <div>
+        {modalOpen ? <CharityModal
+          name={charity.name}
+          img={charity.image}
+          description={charity.description}
+        /> : null}
+      </div>
+    </React.Fragment>
   }
 
   render() {
-    <div className="gallery-title">
-      This bundle supports
-    </div>
-    <div className="gallery-images">
-
-    </div>
-  }
+    const charities = this.props.charities;
+    return(
+      <React.Fragment>
+        <div className="gallery-title">
+          This bundle supports
+        </div>
+        <div className="gallery-images">
+          {charities.map((charity, idx) => {
+            return <li key={idx}>
+              <img
+                src={charity.image}
+                onClick={() => toggleModal(charity.id)}
+              ></img>
+            </li>
+          })}
+        </div>
+      </React.Fragment>
+    )
+  };
 
 }
-
-// Prior code
-/*
-const CharityList = ({ charities, toggleBox }) => (
-  <ul>
-  {charities.map((charity, idx) => {
-    return <li>
-      <img
-        src={charity.image}
-        onClick={() => toggleBox(idx)}
-        style={{ width: '400px', margin: '5px 10px 5px 10px', boxShadow: '0px 0px 10px black'}}
-      ></img>
-    </li>
-  })}
-  </ul>
-);
-*/
 
 export default CharityList;
--- client/components/CharityModal.jsx
+++ client/components/CharityModal.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-
-// this is a stateless component
 class CharityModal extends React.Component {
   render() {
     <div className="modal-container">
       <div className="charity-title">
-
+        {this.props.name}
       </div>
-      <div className="charity-image">
-
-      </div>
+      <div className="charity-image" url={this.props.image}></div>
       <div className="charity-description">
-
+        {this.props.description}
       </div>
     </div>
   }
 }
+
+export default CharityModal;
