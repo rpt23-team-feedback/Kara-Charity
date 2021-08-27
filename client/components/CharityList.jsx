5fbf75539916f5dda10b098513a2f20461d5c4ad
Kara Raithe <55517663+kraithe@users.noreply.github.com>
2021-08-27 16:59:51 -0400

    Add functionality for modal toggle and props

--- client/components/CharityList.jsx
+++ client/components/CharityList.jsx
@@ -1,41 +1,29 @@
 import React from 'react';
 import CharityModal from './CharityModal.jsx';
 
-// 1. Render the 3 images of charities on the main page
-// 2. On click, show the modal for a specific charity
-
-// props - all 1-3 charities, all their info!
-
 class CharityList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modalOpen: false
+      modalOpen: false,
+      charityData: null
     }
     this.toggleModal = this.toggleModal.bind(this);
   }
 
-  // Conditionally render CharityModal
   toggleModal(i) {
     const modalBool = this.state.modalOpen;
-    this.setState({ modalOpen: !modalBool});
-  }
-
-  openModal() {
-    <React.Fragment>
-      <div>
-        {modalOpen ? <CharityModal
-          name={charity.name}
-          img={charity.image}
-          description={charity.description}
-        /> : null}
-      </div>
-    </React.Fragment>
+    this.setState({
+      modalOpen: !modalBool,
+      charityData: this.props.charities[i]
+    });
   }
 
   render() {
     const charities = this.props.charities;
-    return(
+    const charityData = this.state.charityData;
+    const open = this.state.open;
+    return (
       <React.Fragment>
         <div className="gallery-title">
           This bundle supports
@@ -45,10 +33,17 @@ class CharityList extends React.Component {
             return <li key={idx}>
               <img
                 src={charity.image}
-                onClick={() => toggleModal(charity.id)}
+                onClick={() => this.toggleModal(idx)}
               ></img>
             </li>
           })}
+        </div>
+        <div>
+          {open ? <CharityModal
+            name={charityData.name}
+            image={charityData.image}
+            description={charityData.description}
+          /> : null}
         </div>
       </React.Fragment>
     )
--- client/components/CharityModal.jsx
+++ client/components/CharityModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 class CharityModal extends React.Component {
   render() {
     <div className="modal-container">
